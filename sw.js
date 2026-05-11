853c86543399b6dd04ef5f63374c37b8d275cd01
yuriRsts <142285543+yuriRsts@users.noreply.github.com>
2026-05-11 12:38:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
+const CACHE_NAME = "calcular-rota-v1.0.0";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("viagem-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
